b8f3bf068c76d811eafa061ac8ec1155ab086ec3
jianjiandandande <1781634181@qq.com>
2017-11-26 16:33:09 +0800

    update

---
+++ src/RemoveDuplicatesfromSortedArray.java
@@ -0,0 +1,37 @@
+/**
+ * Created by Vincent on 2017/11/26.
+ */
+public class RemoveDuplicatesfromSortedArray {
+
+    public static void main(String[] args){
+
+        RemoveDuplicatesfromSortedArray rdfsa = new RemoveDuplicatesfromSortedArray();
+
+        System.out.println(rdfsa.getRealLength(new int[]{1,2,3,3,4,5,5}));
+
+    }
+
+    /**
+     *
+     * 这里有个坑，一开始没注意到，想了半天没想清楚
+     * i++,先使用，后++
+     * ++i,先++，后使用
+     * @param nums
+     * @return
+     */
+    private int getRealLength(int[] nums){
+
+        int len = nums.length;
+        if (len <= 1) return len;
+        int tail = 1;
+        for (int i = 1; i < len; ++i) {
+            if (nums[i - 1] != nums[i]) {
+                nums[tail++] = nums[i];
+            }
+        }
+        for (int  a: nums) {
+            System.out.println(a);
+        }
+        return tail;
+    }
+}
---
+++ src/ValidParentheses.java
@@ -0,0 +1,44 @@
+/**
+ * Created by Vincent on 2017/11/26.
+ *
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if
+ * the input string is valid.The brackets must close in the correct order, "()" and "()[]{}" are
+ * all valid but "(]" and "([)]" are not.
+ *
+ * 题意是判断括号匹配是否正确
+ *
+ */
+public class ValidParentheses {
+
+    public static void main(String[] args){
+
+        ValidParentheses vp = new ValidParentheses();
+
+        System.out.println(vp.checkIsMate("({sfhjdhfsdjkfh})"));
+
+    }
+
+    /**
+     * 可以通过栈的方式来解决这个问题
+     * 当出现左括号的时候入栈，当遇到右括号时，判断栈顶的左括号是否何其匹配，不匹配的话直接返回false即可
+     * @param s
+     * @return
+     */
+    private boolean checkIsMate(String s){
+        char[] stack = new char[s.length() + 1];
+        int top = 1;
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack[top++] = c;
+            } else if (c == ')' && stack[--top] != '(') {
+                return false;
+            } else if (c == ']' && stack[--top] != '[') {
+                return false;
+            } else if (c == '}' && stack[--top] != '{') {
+                return false;
+            }
+        }
+        return top == 1;
+    }
+
+}
